cb907ef5e651078fc91f0b98315813390d33ffa8
unknown <manaladbryantray_bsit@plmun.edu.ph>
2025-02-20 00:01:20 -0800

    Submitting Python Week 04 - Laboratory # 08

---
+++ if_statement_basic.py
@@ -0,0 +1,12 @@
+# Bryant Ray Manalad
+# ITELEC2
+# Laboratory #06 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    temperature = 35  # Or any other number
+    if temperature > 30:
+        print("It's a hot day!")  # Indented 4 spaces
+
+if __name__ == "__main__":
+    main()
